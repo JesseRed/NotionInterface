56d623193c2a8727f1e49907588945e2b5cd8edf
JesseRed <carsten.klingner@gmail.com>
2021-05-26 01:19:04 +0200

    get working

--- test.py
+++ test.py
@@ -40,11 +40,74 @@ mydata =  {"parent": { "database_id": "385fcd6024784d89965537c8d60ef1cd" },
 # j= r.json()
 # print(j['response'])
 
-r = requests.post('https://api.notion.com/v1/pages', headers=headers, json = mydata)
+#r = requests.post('https://api.notion.com/v1/pages', headers=headers, json = mydata)
+
+
+#import requests
+
+url = "https://api.notion.com/v1/databases/385fcd6024784d89965537c8d60ef1cd/query"
+url = "https://api.notion.com/v1/databases/385fcd6024784d89965537c8d60ef1cd/query"
+
+payload="{\"filter\": {\"property\": \"ID\",\"number\": {\"greater_than_or_equal_to\": 2}}}"
+payload="{\"page_size\": 1}"
+headers = {
+  'Authorization': 'Bearer secret_Bg2w5W0fRJyYgAHl8huffd4ERlgs3VJ1OohTFG8I4mt',
+  'Content-Type': 'application/json',
+  'Notion-Version': '2021-05-13'
+}
+
+#response = requests.request("POST", url, headers=headers, data=payload)
+
+#print(response.text)
+
+
+#print(dir(response))
+#j = response.json()
+#results = j["results"]
+#for i in results:
+#    print(f"--- {i}")
+#print(j["has_more"])
+
+
+# reads one entry after the other
+response = requests.request("POST", url, headers=headers, data=payload)
+j = response.json()
+print("first line --------------")
+print(j["results"])
+while j["has_more"]:
+    print("has_more ...........")
+    x = j["next_cursor"]
+    print(f"next_cursor = {x}")
+    
+    payload="{\"page_size\": 1, \"start_cursor\": " + x + "}"
+    payload="{\"page_size\": 1, \"start_cursor\": \"" + x + "\"}"
+    print(f"payload = {payload}")
+    response = requests.request("POST", url, headers=headers, data=payload)
+    j = response.json()
+    print(j)
+    #print(j["results"])
+
+#print(j["next_cursor"])
+#print(response.status_code)
+#myit = response.iter_lines()
+#print(next(myit))
+#print(next(myit))
+
+##print(response.iter_lines)
+#print(response.iter_content())
+# headers = {
+#   'Authorization': 'Bearer secret_Bg2w5W0fRJyYgAHl8huffd4ERlgs3VJ1OohTFG8I4mt',
+#   'Content-Type': 'application/json',
+#   'Notion-Version': '2021-05-13'
+# }
+
+# response = requests.request("POST", url, headers=headers, data=payload)
+
+# print(response.text)
 
 #r = requests.post('https://api.notion.com/v1/pages', headers=headers)
 
-print(f"r={r}")
+#print(f"r={r}")
 # secret_Bg2w5W0fRJyYgAHl8huffd4ERlgs3VJ1OohTFG8I4mt
 
 # https://www.notion.so/b18b1fc03f2544e68cc83ae317530e90?v=3820dad39d8744eea153b6688c5e20c6
